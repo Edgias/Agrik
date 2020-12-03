30721868c95b45d88a16cc206054a6b2a4557ee5
Edgias Mudzukwa <mudzukwae@outlook.com>
2020-12-03 12:13:34 +0200

    Added projects for unit, functional, and integration tests.

---
+++ FunctionalTests/APIs/CropCategoryEndPoints/CreateEndPoint.cs
@@ -0,0 +1,55 @@
+﻿using Edgias.Agrik.API.Models.Form;
+using Edgias.Agrik.API.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Edgias.Agrik.FunctionalTests.APIs.CropategoryEndPoints
+{
+    [Collection("Sequential")]
+    public class CreateEndPoint : IClassFixture<ApiTestFixture>
+    {
+        JsonSerializerOptions _jsonOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        
+        private string _testDescription = "test description";
+        private string _testName = "test name";
+
+        public CreateEndpoint(ApiTestFixture factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        public HttpClient Client { get; }
+
+        [Fact]
+        public async Task ReturnsSuccessGivenValidNewItemAndAdminUserToken()
+        {
+            StringContent jsonContent = GetValidNewItemJson();
+
+            HttpResponseMessage response = await Client.PostAsync("api/catalog-items", jsonContent);
+            response.EnsureSuccessStatusCode();
+            string stringResponse = await response.Content.ReadAsStringAsync();
+            var model = stringResponse.FromJson<CropCategoryApiModel>();
+
+            Assert.Equal(_testDescription, model.CatalogItem.Description);
+            Assert.Equal(_testName, model.CatalogItem.Name);
+        }
+
+        private StringContent GetValidNewItemJson()
+        {
+            CropCategoryFormApiModel request = new CropCategoryFormApiModel()
+            {
+                Description = _testDescription,
+                Name = _testName
+            };
+
+            StringContent jsonContent = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+
+            return jsonContent;
+        }
+    }
+}
---
+++ FunctionalTests/ApiTestFixture.cs
@@ -0,0 +1,74 @@
+﻿using API;
+using Edgias.Agrik.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Edgias.Agrik.FunctionalTests
+{
+    public class ApiTestFixture : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddEntityFrameworkInMemoryDatabase();
+
+                // Create a new service provider.
+                var provider = services
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                // Add a database context (ApplicationDbContext) using an in-memory 
+                // database for testing.
+                services.AddDbContext<AgrikDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(provider);
+                });
+
+                //services.AddDbContext<AppIdentityDbContext>(options =>
+                //{
+                //    options.UseInMemoryDatabase("Identity");
+                //    options.UseInternalServiceProvider(provider);
+                //});
+
+                // Build the service provider.
+                var sp = services.BuildServiceProvider();
+
+                // Create a scope to obtain a reference to the database
+                // context (ApplicationDbContext).
+                using var scope = sp.CreateScope();
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<AgrikDbContext>();
+                var loggerFactory = scopedServices.GetRequiredService<ILoggerFactory>();
+
+                var logger = scopedServices
+                    .GetRequiredService<ILogger<ApiTestFixture>>();
+
+                // Ensure the database is created.
+                db.Database.EnsureCreated();
+
+                //try
+                //{
+                //    // Seed the database with test data.
+                //    CatalogContextSeed.SeedAsync(db, loggerFactory).Wait();
+
+                //    // seed sample user data
+                //    var userManager = scopedServices.GetRequiredService<UserManager<ApplicationUser>>();
+                //    var roleManager = scopedServices.GetRequiredService<RoleManager<IdentityRole>>();
+                //    AppIdentityDbContextSeed.SeedAsync(userManager, roleManager).Wait();
+                //}
+                //catch (Exception ex)
+                //{
+                //    logger.LogError(ex, $"An error occurred seeding the " +
+                //        "database with test messages. Error: {ex.Message}");
+                //}
+            });
+        }
+    }
+}
